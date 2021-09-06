77ab15abfe57bca0e65c0bf07edb422a76dee7c3
ttejeda <ttejeda@ucol.mx>
2021-09-06 14:40:07 -0500

    implemented board, added snakes and ladders

--- snakesLadders.js
+++ snakesLadders.js
@@ -19,5 +19,16 @@ class Player{
     }
 }
 
+let board = new Array;
+for(let i = 0; i <= 100; i++){
+    board.push(i);
+}
+let snakes = [1, 17, 20, 39, 5, 31, 15, 48, 52];
+let ladders = [8, 22, 48, 95, 65, 76, 100, 82, 94];
+for(let i = 1; i < 10; i++){
+    board[11 * i] = snakes[i-1];
+    board[12 * (i-1) + 2] = ladders[i-1];
+}
+
 let carlos = new Player("Carlos");
 let juan = new Player("Juan");
